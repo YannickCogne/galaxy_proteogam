7fb9b0d8776a630c783080e2bf734bc29a42cbad
YannickCogne <yannick.cogne@hotmail.fr>
2016-12-08 08:40:17 +0100

    Delete docker_pulling.sh

--- docker_pulling.sh
+++
@@ -1,7 +0,0 @@
-docker pull apetkau/smalt-galaxy
-docker pull ycogne/bridger:2014-12-01 
-docker pull ycogne/trinity:2.2.0 
-docker pull ycogne/binpacker:1.1
-docker pull ycogne/oases:0.2.08 
-docker pull ycogne/spades:3.9.0
-docker pull ycogne/transrate:1.0.3
